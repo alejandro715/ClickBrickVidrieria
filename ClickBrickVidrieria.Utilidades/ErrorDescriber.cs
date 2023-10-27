80fe44ce0d6015aa51495f322d63369780544b7a
alejandro715 <a7bautista@gmail.com>
2023-10-27 00:48:56 -0600

    administracion de usuarios

---
+++ ClickBrickVidrieria.Utilidades/ErrorDescriber.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickBrickVidrieria.Utilidades
+{
+    public class ErrorDescriber: IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+
+                Code = nameof(PasswordRequiresLower),
+                Description = "El Password debe tener al menos una letra minuscula"
+            };
+        }
+
+    }
+}
